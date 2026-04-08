8f6e8ba6edb6e30452983172413375a07dda1abe
MatthewGSAE <136556271+MatthewGregory421@users.noreply.github.com>
2026-04-08 15:01:43 +1000

    Revert "Pause Menu Implementation"
    This reverts commit 81b9f73305da6f3027518a716eb14fc97f676d5f.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,49 +4,9 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public MusicManager musicManager;
-    public GameObject menuPanel;
-    private bool isPaused = false;
     public void LoadScene()
     {
         musicManager.StopMusEmitter();
         SceneManager.LoadScene("TestScene");
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            if (!isPaused)
-            {
-                // Open menu and pause
-                menuPanel.SetActive(true);
-                Time.timeScale = 0f;
-                isPaused = true;
-            }
-            else
-            {
-                // Close menu and resume
-                menuPanel.SetActive(false);
-                Time.timeScale = 1f;
-                isPaused = false;
-            }
-        }
-    }
-
-    public void ResumeGame()
-    {
-        isPaused = false;
-        menuPanel.SetActive(false);
-        Time.timeScale = 1f;
-    }
-
-    // Main Menu button (can be called from UI Button OnClick)
-    public void GoToMainMenu()
-    {
-        // Make sure timeScale is reset before loading main menu
-        Time.timeScale = 1f;
-
-        // Replace "MainMenuScene" with your actual main menu scene name
-        SceneManager.LoadScene("MainMenu");
     }
 }
